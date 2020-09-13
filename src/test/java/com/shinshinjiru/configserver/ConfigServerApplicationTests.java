29f78a4ba1c0eb680100c1130178207d57dae69d
manulaiko <manulaiko@gmail.com>
2020-09-13 18:39:52 +0200

    Initial commit

---
+++ src/test/java/com/shinshinjiru/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shinshinjiru.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
